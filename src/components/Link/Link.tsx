1d18028c071f1b9f716dc66707d4c2129c45a970
EfraimCancio <efraimfonseca@gmail.com>
2022-08-29 14:33:22 -0300

    final

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Link.css';
 
-const Link = (props) => {
+const Link = (props: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
     return (
         <>
             <button className="link-button">
